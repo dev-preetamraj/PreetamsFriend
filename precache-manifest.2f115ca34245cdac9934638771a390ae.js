b791a69f2cf11895aef74c544645f1a6cd867052
Preetam Raj <iitianpreetam@gmail.com>
2020-04-14 18:29:17 +0530

    Updates

---
+++ precache-manifest.2f115ca34245cdac9934638771a390ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b698af41f7b1399c471170af78eda60",
+    "url": "/PreetamsFriend/index.html"
+  },
+  {
+    "revision": "156a3a0f57d2ffa1ac67",
+    "url": "/PreetamsFriend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "327b2787cef1f1e08de3",
+    "url": "/PreetamsFriend/static/css/main.b2ad7ffd.chunk.css"
+  },
+  {
+    "revision": "156a3a0f57d2ffa1ac67",
+    "url": "/PreetamsFriend/static/js/2.38d4e27b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PreetamsFriend/static/js/2.38d4e27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327b2787cef1f1e08de3",
+    "url": "/PreetamsFriend/static/js/main.7220637f.chunk.js"
+  },
+  {
+    "revision": "abfae180458e25688903",
+    "url": "/PreetamsFriend/static/js/runtime-main.39d387cb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PreetamsFriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
